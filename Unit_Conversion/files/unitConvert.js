f3a8cd597aafaa94d29e4ef2b745c16d617f4516
mkallgren08 <mkallgren08@gmail.com>
2019-07-11 20:22:40 -0400

    Made some minor adjustments to snippet file

--- Unit_Conversion/files/unitConvert.js
+++ Unit_Conversion/files/unitConvert.js
@@ -1,22 +1,29 @@
 
 let unitConvert = {
-  CtF: (val)=>convertTemp('cel','far',val),
-  CtK: (val)=>convertTemp('cel','kel',val),
-  FtC: (val)=>convertTemp('far','cel',val),
-  FtK: (val)=>convertTemp('far','kel',val),
-  KtC: (val)=>convertTemp('kel','cel',val),
-  KtF: (val)=>convertTemp('kel','far',val),
+  temp: {
+    CtF: (val)=>convertTemp('cel','far',val),
+    CtK: (val)=>convertTemp('cel','kel',val),
+    FtC: (val)=>convertTemp('far','cel',val),
+    FtK: (val)=>convertTemp('far','kel',val),
+    KtC: (val)=>convertTemp('kel','cel',val),
+    KtF: (val)=>convertTemp('kel','far',val),
+  },
+  length: {
+
+  }
+
 };
+
 
 let errorMsg = "Error: unit input was not a number; unit input was - "
 
 // These are various functions for converting one unit of 
 // temperature (either K, C, or F) into another of the three types
 convertTemp = (inp, out, val) => {
+  let validVals= ['kel','cel','far'];
   //console.log(inp,out,val)
   if (isNaN(parseFloat(val))) { return `${errorMsg} ${typeof val}` };
-  if ((inp === 'kel' || inp === 'cel' || inp === 'far') && ( out === 'kel' || out === 'cel' || out === 'far')) {/*console.log("Units are good")*/ }
-  else { return `Temperature unit names not recognized` };
+  if (validVals.indexOf(inp) === -1 || validVals.indexOf(out)=== -1) {return `Temperature unit names not recognized`}
   let res = 0;
   switch (inp) {
     case 'kel':
@@ -32,6 +39,38 @@ convertTemp = (inp, out, val) => {
       return 'The temperature unit conversion you are looking for has not been written yet.'
   };
   return parseFloat(res);
+}
+
+// Use for converting lenghts
+lengthConvert = (inp,out,val) => {
+  if (isNaN(parseFloat(val))) { return `${errorMsg} ${typeof val}` };
+  // List of metric multiplication factors
+  let metMultFact = {
+    // TtoG: Math.pow(10,3),
+    // TtoM: Math.pow(10,6),
+    // Ttok: Math.pow(10,9),
+    // Ttoh: Math.pow(10,10),
+    // Ttoda: Math.pow(10,11),
+    // Ttom: Math.pow(10,12),
+    // Ttod: Math.pow(10,13),
+    // Ttoc: Math.pow(10,14),
+    // Ttom: Math.pow(10,15),
+    // Ttomicro: Math.pow(10,18),
+    // Tton: Math.pow(10,21),
+    // Ttop: Math.pow(10,24),
+    toT: Math.pow(10,12),
+    toG: Math.pow(10,9),
+    toM: Math.pow(10,6),
+    tok: Math.pow(10,3),
+    toh: Math.pow(10,2),
+    toda: Math.pow(10,1),
+    tod: Math.pow(10,-1),
+    toc: Math.pow(10,-2),
+    tom: Math.pow(10,-3),
+    tomicro: Math.pow(10,-6),
+    ton: Math.pow(10,-9),
+    top: Math.pow(10,-12)
+  }
 }
 
 // export {unitConvert};
