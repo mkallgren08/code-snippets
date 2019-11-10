c26f35e0cc08ede3f207c6e40b6283b18b5b65df
mkallgren08 <mkallgren08@gmail.com>
2019-11-10 10:01:16 -0500

    added new commands to build 3D printer files and moved files into the code repo for tracking

---
+++ Reading_Files/XMLRequestRead.js
@@ -0,0 +1,32 @@
+const readline = require('readline');
+const XMLHttpRequest = require('xmlhttprequest').XMLHttpRequest;
+
+let path = null;
+
+process.argv[2]?path=process.argv:null;
+
+loadFile = (path) => {
+  let res = null;
+  const xmlhttp = new XMLHttpRequest();
+  xmlhttp.open("GET", path, false);
+  xmlhttp.send();
+  xmlhttp.status===200?res=xmlhttp.responseText:null;
+  return res;
+}
+// path?console.log(loadFile(path)):console.log('Input "./test.txt"')
+
+// export default loadFile;
+
+// loadFile = (path) => {
+//     let res = null;
+//     const reader = new FileReader();
+//     reader.onload = (e) => {
+//       let contents = e.target.result;
+//       console.log(contents);
+//     }
+//     reader.send();
+//     reader.status===200?res=xmlhttp.responseText:null;
+//     return res;
+// }
+
+path?console.log(loadFile(path)):console.log('Input "./test.txt"')
